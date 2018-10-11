176b27d84138b253d0ddb7424afb52d065c32673
NightRobot <alseus13@hotmail.com>
2018-10-11 16:23:41 +0700

    test pulp library

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
-import numpy as np
-a = np.array([1,2,3,4])
-print(a)
+import pulp
+# Instantiate our problem class
+model = pulp.LpProblem("Profit maximising problem", pulp.LpMaximize)
+A = pulp.LpVariable('A', lowBound=0, cat='Integer')
+B = pulp.LpVariable('B', lowBound=0, cat='Integer')
+# Objective function
+model += 30000 * A + 45000 * B, "Profit"
 
-print(np.delete(a,1))
-np.append(a, 4)
-a = np.append([1, 2, 3], [[4, 5, 6], [7, 8, 9]])
-print(a)
+# Constraints
+model += 3 * A + 4 * B <= 30
+model += 5 * A + 6 * B <= 60
+model += 1.5 * A + 3 * B <= 21
+# Solve our problem
+model.solve()
+pulp.LpStatus[model.status]
+# Print our decision variable values
+print("Production of Car A = {}".format(A.varValue))
+print("Production of Car B = {}".format(B.varValue))
+
+# Print our objective function value
+print(pulp.value(model.objective))
